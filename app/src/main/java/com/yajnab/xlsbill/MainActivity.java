3e3b18ffee05ac2d0a1fdd8dccdf52b79547a3d8
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2015-04-17 22:17:46 +0530

    Initial Commmit

---
+++ app/src/main/java/com/yajnab/xlsbill/MainActivity.java
@@ -0,0 +1,107 @@
+package com.yajnab.xlsbill;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.widget.Toast.LENGTH_LONG;
+
+public class MainActivity extends ActionBarActivity {
+
+    private View.OnClickListener excelWriter;
+    {
+        excelWriter = new View.OnClickListener() {
+            public void onClick(View v) {
+                exwrite();
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(excelWriter);
+
+    }
+
+    public void exwrite() {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet firstSheet = workbook.createSheet("MY FIRST SHEET");
+        HSSFSheet secondSheet = workbook.createSheet("MY SECOND SHEET");
+
+        HSSFRow rowA = firstSheet.createRow(0);
+        HSSFCell cellA = rowA.createCell(0);
+        cellA.setCellValue(new HSSFRichTextString("MY FIRST SHEET"));
+
+        HSSFRow rowB = secondSheet.createRow(0);
+        HSSFCell cellB = rowB.createCell(0);
+        cellB.setCellValue(new HSSFRichTextString("MY SECOND SHEET"));
+
+        FileOutputStream fos = null;
+        try {
+
+            String str_path = Environment.getExternalStorageDirectory().toString();
+            File file;
+            file = new File(str_path, getString(R.string.app_name) + ".xls");
+            fos = new FileOutputStream(file);
+            Toast.makeText(MainActivity.this, "Done", LENGTH_LONG).show();
+
+
+            workbook.write(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "" + e, LENGTH_LONG).show();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.flush();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(MainActivity.this, "File Generated", LENGTH_LONG).show();
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
